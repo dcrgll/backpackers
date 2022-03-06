84aef1a923dacc65af15edbefbcbf7568e924a85
dan <hi@dancargill.uk>
2022-03-06 16:06:23 +0000

    Remove warnings on seed

--- src/config/seed.js
+++ src/config/seed.js
@@ -12,15 +12,22 @@ async function getSeedData () {
   try {
     ;[...Array(10).keys()].map(() =>
       db.collection('users').add({
-        username: faker.internet.userName(),
         first_name: faker.name.firstName(),
         last_name: faker.name.lastName(),
         avatar: faker.image.imageUrl(),
-        email: faker.internet.email()
+        address: {
+          addr_1: faker.address.streetAddress(),
+          addr_2: faker.address.secondaryAddress(),
+          city: faker.address.city(),
+          state: faker.address.state(),
+          zipCode: faker.address.zipCode()
+        }
       })
     )
+    // eslint-disable-next-line no-console
     console.log('database seed was successful')
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.log(error, 'database seed failed')
   }
 }
