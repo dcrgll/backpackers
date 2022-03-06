aa9867fa59ba2dc86d94bd7473169f105a505b65
dan <hi@dancargill.uk>
2022-03-06 19:29:46 +0000

    Fix standard formatting

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -4,9 +4,9 @@ import Footer from '../foooter'
 export default function Layout ({ children, background }) {
   return (
     <>
-      <header className="h-12 bg-red-400 sticky top-0"></header>
+      <header className='h-12 bg-red-400 sticky top-0' />
       <main className={`${background}`}>
-        <div className="container mx-auto px-4">{children}</div>
+        <div className='container mx-auto px-4'>{children}</div>
       </main>
       <Footer />
     </>
